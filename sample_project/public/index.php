e2b7bac8e8b6a235eb250e9cc0b4b707bd382359
Bill Lee <github@evolutedesign.com>
2017-12-02 17:06:49 -0800

    Fix syntax typo

--- sample_project/public/index.php
+++ sample_project/public/index.php
@@ -20,7 +20,7 @@
 is_file('local_defs.php') && include('local_defs.php');
 
 // If APP_DIR defined and is valid,
-if ( defined('APP_DIR') && is_file(APP_DIR . DIRECTORY_SEPARATOR . 'app.php') ) {
+if ( defined('APP_DIR') && is_file(APP_DIR . DIRECTORY_SEPARATOR . 'app.php') )
 	require_once(APP_DIR . DIRECTORY_SEPARATOR . 'app.php');
 elseif ( is_file(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'app.php') )
 	// Default, if prior test fails assume parent dir contains app.php (as with symlink)
